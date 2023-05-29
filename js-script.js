7c1d934a039a474d4202728fbcec3ffd99c33b39
Tas <tasudomsiri@gmail.com>
2023-05-29 16:15:48 +0700

    ck

--- js-script.js
+++ js-script.js
@@ -1,8 +1,8 @@
-const addButton = document.getElementById('add');
-const input = document.getElementById('userInput');
+const addButton = document.getElementById("add");
+const input = document.getElementById("userInput");
 
-const ul = document.querySelector('ul');
-const item = document.getElementsByTagName('li');
+const ul = document.querySelector("ul");
+const item = document.getElementsByTagName("li");
 
 const ENTER_BUTTON_CODE = 13;
 
@@ -14,27 +14,46 @@ function listLength() {
   return item.length;
 }
 
+let taskArr = [];
 function createListElement() {
   // step1: create new li element
-  // const newLiElement = document.createElement('li');
+  const newLiElement = document.createElement("li");
 
   // step2: get input value
+  let inputValue = input.value;
+  const inputNode = document.createTextNode(inputValue);
+  newLiElement.appendChild(inputNode);
+
+  // saving to localstorage
+  taskArr.push(input.value);
+  console.log(taskArr);
+  localStorage.setItem("task", JSON.stringify(taskArr));
 
   // step3: append element to ul and then reset input field
+  input.value = "";
 
   function crossOut() {
-    newLiElement.classList.toggle('todos--done');
+    newLiElement.classList.toggle("todos--done");
   }
 
+  newLiElement.addEventListener("click", crossOut);
+
   // step4: add click event listener to new li
+  ul.appendChild(newLiElement);
 
   // step5: create delete button and append intp new li
+  let deleteBtn = document.createElement("button");
+  // deleteBtn.appendChild(document.createTextNode("X"));
+  const deleteIcon = "<i class='fa-regular fa-circle-xmark fa-lg'></i>";
+  deleteBtn.innerHTML = deleteIcon;
+  newLiElement.appendChild(deleteBtn);
 
   function deleteListItem() {
-    newLiElement.classList.add('todos--delete');
+    newLiElement.classList.add("todos--delete");
   }
 
   // step6: add click event listener to delete button
+  deleteBtn.addEventListener("click", deleteListItem);
 }
 
 function addListAfterClick() {
@@ -49,5 +68,5 @@ function addListAfterKeypress(event) {
   }
 }
 
-addButton.addEventListener('click', addListAfterClick);
-input.addEventListener('keypress', addListAfterKeypress);
+addButton.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
